6e571d210f4b914a4452ad51def81c77d1254519
miguells92 <117354338+miguells92@users.noreply.github.com>
2022-11-09 14:59:35 +0100

    Update ML.java

--- git_Miguel_Lorente/src/git_Miguel_Lorente/ML.java
+++ git_Miguel_Lorente/src/git_Miguel_Lorente/ML.java
@@ -4,10 +4,10 @@ public class ML {
 
 	public static void main(String[] args) {
 		
-
+		System.out.println("Alumno: Miguel Lorente");	
 		System.out.println("Modificación desde Master - Cambiado");
+		System.out.println("Modificacion desde Master");
 		System.out.println("Modificacion desde Dev");
-
 
 	}
 
